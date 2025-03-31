4671ec390e774321e2da8a49913422199b0f84e5
Ethan-Aldridge <60679776+Ethan-Aldridge@users.noreply.github.com>
2025-03-31 17:23:53 -0500

    Delete database directory

--- database/testdb/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Battery
-
-admin.site.register(Battery)
--- database/testdb/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from testdb import views
-
-urlpatterns = [
-    path("", views.home, name="home"),
-    path("inventory/", views.inventory, name="inventory"),
-]
