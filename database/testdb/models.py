34a9b940ba9dbd7cb2a4e210aede5f954cc398f6
Ethan-Aldridge <ethan.a@ou.edu>
2025-04-27 20:33:01 -0500

    Fixed null date handling

--- database/testdb/models.py
+++ database/testdb/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.timezone import now
 
 class Battery(models.Model):
     serial_num = models.IntegerField(primary_key=True)
@@ -6,8 +7,8 @@ class Battery(models.Model):
     item_type = models.CharField(max_length=60)
     user_ID = models.CharField(max_length=80)
     location = models.CharField(max_length=80)
-    date_added = models.DateTimeField(auto_now_add=True, null=True)
-    last_modified = models.DateTimeField(auto_now=True, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
     description = models.TextField()
     madlibs = models.TextField()
     image = models.ImageField(upload_to='inventory/images/') # TODO: fix saving and display of images
