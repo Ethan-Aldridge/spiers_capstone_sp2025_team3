9e544f9a2ddbd57210914ef527d66c088f7761dc
Ethan-Aldridge <ethan.a@ou.edu>
2025-04-27 20:06:21 -0500

    Images supported, remote posting handled

--- database/testdb/models.py
+++ database/testdb/models.py
@@ -6,11 +6,11 @@ class Battery(models.Model):
     item_type = models.CharField(max_length=60)
     user_ID = models.CharField(max_length=80)
     location = models.CharField(max_length=80)
-    date_added = models.DateTimeField(auto_now_add=True)
-    last_modified = models.DateTimeField(auto_now=True)
+    date_added = models.DateTimeField(auto_now_add=True, null=True)
+    last_modified = models.DateTimeField(auto_now=True, null=True)
     description = models.TextField()
     madlibs = models.TextField()
-    image = models.ImageField(upload_to='images/') # TODO: fix saving and display of images
+    image = models.ImageField(upload_to='inventory/images/') # TODO: fix saving and display of images
 
 class WarehouseA(models.Model):
     serial_num = models.IntegerField(primary_key=True)
